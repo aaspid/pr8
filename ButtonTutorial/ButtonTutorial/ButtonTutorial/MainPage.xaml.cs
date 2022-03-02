012ee1320ab9b2f36d7cb3e38fbed7e8effb3770
irinaaspidova0@gmail.com <irinaaspidova0@gmail.com>
2022-03-03 00:49:09 +0300

    Добавьте файлы проекта.

---
+++ ButtonTutorial/ButtonTutorial/ButtonTutorial/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ButtonTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : FlyoutPage
+    {
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+    }
+}
